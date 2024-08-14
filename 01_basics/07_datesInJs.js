8cf8ff2de8b21485da94be9b3471e7f4e4f88f6c
joshirohit407 <70706077+joshirohit407@users.noreply.github.com>
2024-08-14 18:15:13 +0000

    date in JS

---
+++ 01_basics/07_datesInJs.js
@@ -0,0 +1,20 @@
+// let myDate = new Date()
+// console.log(myDate);
+// console.log(myDate.toString());
+// console.log(myDate.toDateString());
+// console.log(myDate.toLocaleDateString());
+// console.log(typeof myDate);                   // Type of date is Object.
+
+// let myCreatedDate = new Date(2024, 0, 26)
+// console.log(myCreatedDate.toDateString())        // Fri Jan 26 2024
+       
+// let myCreatedDate = new Date(2024, 0, 26, 5, 3)
+// console.log(myCreatedDate.toLocaleString())         //   1/26/2024, 5:03:00 AM
+
+
+let myCreatedDate = new Date("01-26-2024")
+// console.log(myCreatedDate.toLocaleString())
+
+
+let myTimeStamp = Date.now()
+console.log(myTimeStamp);
