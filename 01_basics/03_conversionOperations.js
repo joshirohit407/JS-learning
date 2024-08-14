3da13431adffc9239a7963456b8453ea2b0d7a43
joshirohit407 <70706077+joshirohit407@users.noreply.github.com>
2024-08-14 07:34:35 +0000

    conversion and operations

---
+++ 01_basics/03_conversionOperations.js
@@ -0,0 +1,49 @@
+let score = "rohit"
+
+//console.log(typeof score);  // string
+//console.log(typeof (score));   // string
+
+let valueInNumber = Number(score)
+//console.log(valueInNumber)     // NaN
+//console.log(typeof valueInNumber)    // number
+
+//"33" => 33
+// "33abc" => NaN (not a number)
+// true => 1; false => 0
+
+let isLoggedIn = "rohit"
+
+let booleanisLoggedIn = Boolean(isLoggedIn)
+//console.log(booleanisLoggedIn)    // true
+
+// 1 => true; 0 => false
+// "" => false
+// "rohit" => true
+
+let someNumber = 45
+
+let stringNumber = String(someNumber)
+//console.log(stringNumber)          //45
+//console.log(typeof stringNumber)  // string
+
+
+// ************************** OPERATIONS *****************************************//
+
+let value = 3
+let negValue = -value;
+//console.log(negValue)  // -3
+
+//console.log(2**3)  // 8      (power)
+//console.log(2%3)   // 2      (remainder)
+
+let str1 = "hello"
+let str2 = " Rohit"
+let str3 = str1 + str2
+//console.log(str3)   // hello Rohit
+
+// console.log("1" +2)  // 12
+// console.log(1 +"2")  // 12
+
+// console.log("1" +2 +2)   // 122  // string is in first then all will be treated as string
+// console.log(1 +2 +"2")   // 32   // string is in last then start will be solved first 
+
