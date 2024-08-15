8723a719df0838b0ebda22df315f6da579b6faab
joshirohit407 <70706077+joshirohit407@users.noreply.github.com>
2024-08-15 17:24:03 +0000

    array

---
+++ 02_basics/01_array.js
@@ -0,0 +1,52 @@
+// Arrays 
+
+// const myArr = [0, 1, 2, 3, 4, 5]
+// const myHeroes = ["Batman", "Superman"]
+
+// // OR we can also declare an array as :
+
+// const myArr2 = new Array(1, 2, 3, 4)
+// console.log(myArr2[2])          // 3
+// console.log(myArr2.length)      // 4
+
+// Array Methods
+
+// const myArr = [1, 2, 3, 4, 5]
+
+// myArr.push(6)             // Adds new element to the last.
+// console.log(myArr)     
+
+// myArr.pop()               // Removes an element from the last.
+// console.log(myArr)
+
+// myArr.unshift(9)           // Adds new element at the start.
+// console.log(myArr)        
+
+// myArr.shift()              // Removes an element from the start.
+// console.log(myArr)
+
+// console.log(myArr.includes(9))       // false
+// console.log(myArr.indexOf(4))        // 3
+
+// const newArr = myArr.join()      // A string used to separate one element of the array from the next in the resulting string. If omitted, the array elements are separated with a comma.
+// console.log(newArr)              // 1, 2, 3, 4, 5
+// console.log(typeof newArr)       // string
+
+
+// const myArr = [1, 2, 3, 4, 5, 6]
+
+// console.log("A", myArr)
+
+// const mynewArr = myArr.slice(1,3)      // Slice gives new array and no changes in the original array.
+
+// console.log(mynewArr);      // [2,3]
+
+// console.log("B", myArr); 
+
+// const mynewArr2 = myArr.splice(1,3)     // splice also gives new array but makes changes in the original arrayl.
+// console.log(mynewArr2)       // [2, 3, 4]
+// console.log("C", myArr)
+
+
+
+
