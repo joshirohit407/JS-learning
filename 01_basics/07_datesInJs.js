729bb65c38553289b9336ddfdc82320f3673a7d6
joshirohit407 <70706077+joshirohit407@users.noreply.github.com>
2024-08-15 06:42:52 +0000

    dates and timestamp in JS

--- 01_basics/07_datesInJs.js
+++ 01_basics/07_datesInJs.js
@@ -12,9 +12,26 @@
 // console.log(myCreatedDate.toLocaleString())         //   1/26/2024, 5:03:00 AM
 
 
-let myCreatedDate = new Date("01-26-2024")
-// console.log(myCreatedDate.toLocaleString())
+// let myCreatedDate = new Date("01-26-2024")
+// // console.log(myCreatedDate.toLocaleString())
 
 
-let myTimeStamp = Date.now()
-console.log(myTimeStamp);
+// let myTimeStamp = Date.now()
+// console.log(myTimeStamp);
+// console.log(myCreatedDate.getTime());
+
+// console.log( Date.now());           //give miliseconds to today's date. to change them to seconds we can divide by 1000.
+
+// console.log(Math.floor( Date.now()/1000))
+
+
+let newDate = new Date()
+console.log(newDate);
+console.log(newDate.getMonth());         // 7  but the month is of August (8), the output is 7 because the months in JS starts with 0.  we can add 1 to get the exact value.
+
+console.log(newDate.getMonth() + 1);     // 8
+
+console.log (newDate.toLocaleString('default', {
+    weekday : "long"
+}))          // Thursday
+
