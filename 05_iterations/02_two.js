f9a819f3f7c3d54c85a8de8696ec5d3e29b5da8c
joshirohit407 <70706077+joshirohit407@users.noreply.github.com>
2024-08-23 18:53:04 +0000

    while and do while loops

---
+++ 05_iterations/02_two.js
@@ -0,0 +1,28 @@
+// while loop :
+
+// let index = 0
+// while (index <=10) {
+//     console.log(`Value of index is ${index}`)
+//     index = index + 2
+// }
+
+
+// while loop in array
+
+// let myArray = ["flash", "batman", "superman"]
+
+// let arr = 0
+// while (arr < myArray.length) {
+//     console.log(`Value is ${myArray[arr]}`)
+//     arr = arr + 1         // or arr ++     (both are same)
+// }
+
+
+
+// do while loop :
+
+let score = 1
+do {
+    console.log(`Score is ${score}`)
+    score++
+} while (score <= 10);
