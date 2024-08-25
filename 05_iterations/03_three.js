9bb6c2cecdab6485218e2a91f058e4a7612f49b5
joshirohit407 <70706077+joshirohit407@users.noreply.github.com>
2024-08-25 07:53:10 +0000

    For of loop

--- 05_iterations/03_three.js
+++ 05_iterations/03_three.js
@@ -9,9 +9,36 @@ for (const num of arr) {
 
 const greetings = "Hello World!"
 for (const greet of greetings) {
-    console.log(`Each character is ${greet}`)
+    //console.log(`Each character is ${greet}`)
 }
 
 
-// Maps
+// Maps   (a type of datatype in JavaScript)
 
+// The Map object holds key-value pairs and remembers the original insertion order of the keys. Any value (both objects and primitive values) may be used as either a key or a value.
+
+const map = new Map()
+
+map.set('IN', 'India')
+map.set('US', 'United States')
+map.set('UK', 'United Kingdom')
+//map.set('IN', 'India')             // This will not print again because map holds unique values.
+
+//console.log(map)
+
+// for of loop in map
+for (const [key, value] of map) {
+    //console.log(key, ':-' ,value )
+}
+
+
+// for of loop on Object     : object is not iterable through "for of" loop.
+
+// const myObj = {
+//     name : "rohit",
+//     age : 28,
+//     place : "khatima"
+// }
+// for (const [key, value] of myObj) {
+//     console.log(key, ':-' ,value )
+// }
